225fda8ce655f148b4ac7bad50033f2a8a45ce10
oguzambalaj <vedat@oguzambalaj.com>
2021-03-17 17:26:38 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from "./routers/AppRouter";
+import { createStore } from "redux";
 import "./App.css";
+
+const initialState = {
+    count: 0
+}
+
+const store = createStore((state = initialState) => {
+    return state;
+});
 
 ReactDOM.render(<AppRouter />, document.getElementById('root')
 );
